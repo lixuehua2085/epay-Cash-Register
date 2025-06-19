00b007bae08a49a72b917c9d00fd46e4995042fd
lixuehua2085 <dannydyf@outlook.com>
2025-06-19 10:14:20 +0800

    Update Cash_Register.php

--- epay-Cash-Register/Cash_Register.php
+++ epay-Cash-Register/Cash_Register.php
@@ -1,17 +1,20 @@
 <?php
 require_once("./config/database.php");
+require_once("./config/Communication_Key.php");
 if( $_SERVER['REQUEST_METHOD'] === 'GET'){
     $trade_name = isset($_GET['trade_name']) ? $_GET['trade_name'] : '';
     $trade_amount = isset($_GET['trade_amount']) ? $_GET['trade_amount'] : '';
     $return_url = isset($_GET['return_url']) ? $_GET['return_url'] : '';
     $return_type = isset($_GET['return_type']) ? $_GET['return_type'] : '';
     $trade_no = isset($_GET['trade_no']) ? $_GET['trade_no'] : '';
+    $sign = isset($_GET['sign']) ? $_GET['sign'] : '';
 }else{
     $trade_name = isset($_POST['trade_name']) ? $_POST['trade_name'] : '';
     $trade_amount = isset($_POST['trade_amount']) ? $_POST['trade_amount'] : '';
     $return_url = isset($_POST['return_url']) ? $_POST['return_url'] : '';
     $return_type = isset($_POST['return_type']) ? $_POST['return_type'] : '';
     $trade_no = isset($_POST['trade_no']) ? $_POST['trade_no'] : '';
+    $sign = isset($_POST['sign']) ? $_POST['sign'] : '';
 }
 if (empty($trade_no)) {
     die("401");
@@ -22,11 +25,17 @@ if (empty($trade_amount)) {
 if (empty($return_url)) {
     die("403");
 }
+if (empty($sign)) {
+    die("404");
+}
 if (empty($return_type)) {
     $return_type = "sync";
 }
 if (empty($trade_name)) {
     $trade_name = "收银台支付";
+}
+if ($sign !== md5($trade_no.$ckey)) {
+    die("405");
 }
 $pdo = new PDO("mysql:host=$serverip;dbname=$dbname", $username, $password);
 $trade_no = htmlspecialchars($trade_no, ENT_QUOTES, 'UTF-8');
